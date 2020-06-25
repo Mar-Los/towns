6adfbcfb0e7251546f74169166d5938f4e830fcb
Martin Losinsky <martin@titanus.cz>
2020-06-26 01:19:07 +0200

    First commit

---
+++ constants.js
@@ -0,0 +1,139 @@
+const states = {
+    "Estonsko": [
+        "Talin", "Tartu", "Narva",
+    ],
+    "Litva": [
+        "Vilnius", "Kaunas", "Klajpeda",
+    ],
+    "Lotyšsko": [
+        "Riga",
+    ],
+    "Ukrajina": [
+        "Kijev", "Oděsa", "Simferopol", "Sevastopol", "Jalta", "Užhorod", "Dněpropetrovsk", "Doněck", "Černobyl",
+    ],
+    "Bělorusko": [
+        "Minsk",
+    ],
+    "Moldavsko": [
+        "Kišiněv",
+    ],
+    "Bulharsko": [
+        "Sofie", "Burgas", "Varna",
+    ],
+    "Rumunsko": [
+        "Bukurešť", "Constanta", "Pitesti", "Cluj", "Arad", "Temešvár",
+    ],
+    "Albánie": [
+        "Tirana",
+    ],
+    "Kosovo": [
+        "Priština",
+    ],
+    "Makedonie": [
+        "Skopje",
+    ],
+    "Černá Hora": [
+        "Podgorica",
+    ],
+    "Bosna a Hercegovina": [
+        "Sarajevo",
+    ],
+    "Chorvatsko": [
+        "Záhřeb", "Dubrovník", "Split",
+    ],
+    "Srbsko": [
+        "Novi Sad", "Bělehrad",
+    ],
+    "Slovinsko": [
+        "Ljubljana", "Maribor",
+    ],
+    "Slovensko": [
+        "Bratislava", "Košice", "Žilina", "Trenčín", "Prešov", "Bánská Bystrica", "Trnava",
+    ],
+    "Maďarsko": [
+        "Budapešť", "Visegrad", "Györ", "Miskolc", "Debrecen", "Szeged",
+    ],
+    "Polsko": [
+        "Varšava", "Gdaňsk", "Štětín", "Krakov", "Wroclaw", "Lodž", "Poznaň",
+    ],
+    "Malta": [
+        "La Valetta",
+    ],
+    "Švýcarsko": [
+        "Bern", "Ženeva", "Lausanne", "Curych",
+    ],
+    "Lichtenštejnsko": [
+        "Vaduz",
+    ],
+    "Norsko": [
+        "Oslo", "Narvik", "Stavanger", "Trondheim", "Bergen",
+    ],
+    "Island": [
+        "Reykjavík",
+    ],
+    "Švédsko": [
+        "Stockholm", "Göteborg", "Malmö", "Uppsala", "Gällivare", "Kiruna",
+    ],
+    "Finsko": [
+        "Helsinki", "Turku", "Tampere", "Rovaniemi",
+    ],
+    "Anglie": [
+        "Londýn", "Leeds", "Newcastle", "Manchester", "Liverpool", "Southampton", "Brighton", "Bristol", "Portsmouth", "Sheffield", "Birmingham", "Oxford", "Cambridge",
+    ],
+    "Severní Irsko": [
+        "Belfast"
+    ],
+    "Irsko": [
+        "Dublin",
+    ],
+    "Wales": [
+        "Cardiff",
+    ],
+    "Skotsko": [
+        "Edinburgh", "Glasgow",
+    ],
+    "Dánsko": [
+        "Kodaň",
+    ],
+    "Nizozemsko": [
+        "Amsterdam", "Den Haag", "Rotterdam", "Naarden", "Leiden", "Utrecht", "Haarlem", "Maastricht", "Eindhoven",
+    ],
+    "Belgie": [
+        "Brusel", "Bruggy", "Antverpy", "Gent", "Charleroi",
+    ],
+    "Lucembursko": [
+        "Lucemburk", "Schengen",
+    ],
+    "Francie": [
+        "Paříž", "Marseille", "Toulouse", "Bordeaux", "Nantes", "Lyon", "Cannes", "Nice", "Strasbourg", "Le Havre", "Lurdy", "Grenoble",
+    ],
+    "Španělsko": [
+        "Madrid", "Santiago de Compostella", "Barcelona", "Valencie", "Cádiz", "Bilbao", "Oviedo", "Salamanca", "Malaga", "Sevilla",
+    ],
+    "Portugalsko": [
+        "Fátima", "Lisabon", "Porto",
+    ],
+    "Itálie": [
+        "Řím", "Milán", "Turín", "Janov", "Benátky", "Terst", "Neapol", "Tarent", "Bari", "Brindisi", "Bologna", "Florencie",
+    ],
+    "Řecko": [
+        "Soluň", "Pireus", "Athény",
+    ],
+    "Německo": [
+        "Berlín", "Hamburk", "Brémy", "Dortmund", "Duisburg", "Essen", "Düsseldorf", "Rostock", "Kiel", "Mnichov", "Drážďany", "Norimberk", "Stuttgart", "Ingolstadt", "Passau", "Lipsko",
+    ],
+    "Rakousko": [
+        "Vídeň", "Linz", "Salzburg", "Innsbruck", "Graz", "Klagenfurt",
+    ]
+}
+let numberOfCities = 0;
+let statesInLowerCase = [];
+
+Object.values(states).forEach(state => {
+    state.forEach(city => {
+        numberOfCities++;
+    })
+})
+Object.keys(states).forEach(state => {
+    statesInLowerCase.push(state.toLowerCase())
+})
---
+++ main.js
@@ -0,0 +1,87 @@
+const setHighScoreCookie = highScore => {
+    const daysToLast = 30
+    let date = new Date()
+    date.setTime(date.getTime() + (daysToLast * 24 * 60 * 60 * 1000))
+    expires = `; expires=${date.toUTCString()}`
+    document.cookie = `highscore=${highScore}${expires}; path=/`
+}
+
+const getHighScoreCookie = _ => {
+    let highScoreCookie = document.cookie
+    if (highScoreCookie.indexOf('highscore=') == 0) return highScoreCookie.substring('highscore='.length, highScoreCookie.length)
+    return null
+}
+
+
+let correctAnswersStreak = document.querySelector('footer').textContent
+let badAnswers = ''
+let rndCity
+let rightAnswer
+
+$(_ => {
+    let highScoreFromCookie = getHighScoreCookie()
+    if (highScoreFromCookie != null) {
+        updateHighScore(highScoreFromCookie)
+        correctAnswersStreak = highScoreFromCookie
+    }
+    rndCity = getRndCity()
+    changeCity(rndCity)
+    $("#btn").click(() => {
+        processAnswer()
+    })
+})
+
+const processAnswer = _ => {
+    check()
+    updateHighScore(correctAnswersStreak)
+    rndCity = getRndCity()
+    changeCity(rndCity)
+}
+
+const changeCity = newCity => {
+    $("#input").val("")
+    $("#town").text(newCity)
+}
+
+function getRndCity() {
+    let statesKeys = Object.keys(states)
+    let rndStateCitiesArr = states[statesKeys[statesKeys.length * Math.random() << 0]]
+    let rndCity = rndStateCitiesArr[rndStateCitiesArr.length * Math.random() << 0]
+    return rndCity
+}
+
+const check = _ => {
+    let input = $("#input").val()
+    let formattedInput = input[0].toUpperCase() + Array.from(input).slice(1).join('').toLowerCase()
+
+    let wasWrong = true
+    for (const [state, cities] of Object.entries(states)) {
+        if (cities.includes(rndCity)) rightAnswer = state
+        if (state == formattedInput && cities.includes(rndCity)) wasWrong = false
+    }
+    wasWrong
+        ? processWrongAnswer(input, rightAnswer)
+        : processCorrectAnswer()
+}
+
+const updateHighScore = newScore => {
+    if ($("footer").text() < newScore) {
+        $("footer").text(newScore)
+        setHighScoreCookie(newScore)
+    }
+}
+
+const processWrongAnswer = (wrongAnswer, correctAnswer) => {
+    let formattedWrongAnswer = wrongAnswer[0].toUpperCase() + Array.from(wrongAnswer).slice(1).join('').toLowerCase()
+    $("#error").css("color", "#c23616")
+    $("#error").text(`${formattedWrongAnswer} Correct answer: ${correctAnswer}`)
+    correctAnswersStreak = 0
+    badAnswers = formattedWrongAnswer + ', '
+    $("section").prepend(badAnswers)
+}
+
+const processCorrectAnswer = _ => {
+    correctAnswersStreak++
+    $("#error").css("color", "#059435")
+    $("#error").text(correctAnswersStreak)
+}
